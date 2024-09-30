d69856c67ae2dbd889d3d6fe34423feaa7b32cc5
christopherverstegenwarren <christopherverstegenwarren@gmail.comgit remote -vgit config --global user.email christopherverstegenwarren@gmail.com>
2024-10-01 01:23:13 +0300

    from time import sleep import pandas as pd from selenium import webdriver from bs4 import BeautifulSoup import os
    driver = webdriver.Chrome()
    
    url = 'https://www.skyscanner.com.tr/transport/flights/ista/dps/241019/?adultsv2=1&cabinclass=economy&childrenv2=&ref=home&rtn=0&preferdirects=false&outboundaltsenabled=false&inboundaltsenabled=false'
    
    driver.get(url)

---
+++ webscrapper.py
@@ -0,0 +1,14 @@
+from time import sleep
+import pandas as pd
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import os
+
+driver = webdriver.Chrome()
+
+url = 'https://www.skyscanner.com.tr/transport/flights/ista/dps/241019/?adultsv2=1&cabinclass=economy&childrenv2=&ref=home&rtn=0&preferdirects=false&outboundaltsenabled=false&inboundaltsenabled=false'
+
+driver.get(url)
+
+
+
