b83ddbdc4883a7b55664b6697b826e83965c91c6
christopherverstegenwarren <christopherverstegenwarren@gmail.comgit remote -vgit config --global user.email christopherverstegenwarren@gmail.com>
2024-10-01 01:40:45 +0300

    testing the sleep

--- webscrapper.py
+++ webscrapper.py
@@ -7,9 +7,8 @@ import os
 driver = webdriver.Chrome()
 
 url = 'https://www.skyscanner.com.tr/transport/flights/ista/dps/241019/?adultsv2=1&cabinclass=economy&childrenv2=&ref=home&rtn=0&preferdirects=false&outboundaltsenabled=false&inboundaltsenabled=false'
-
+sleep(10)
 driver.get(url)
-
 
 
 
